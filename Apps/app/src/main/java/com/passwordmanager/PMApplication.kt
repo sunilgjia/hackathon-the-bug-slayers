d31fd1357d3b75a48f8fa9993a521478b95c3687
Keyuri Bhatelia <keyuri.bhatelia@staqo.com>
2021-09-25 15:01:21 +0530

    fixed dagger issue

--- Apps/app/src/main/java/com/passwordmanager/PMApplication.kt
+++ Apps/app/src/main/java/com/passwordmanager/PMApplication.kt
@@ -1,16 +1,26 @@
 package com.passwordmanager
 
 import android.app.Application
+import com.passwordmanager.di.DaggerPMComponent
+import com.passwordmanager.di.PMComponent
+import com.passwordmanager.di.PMComponentProvider
 import com.passwordmanager.shared.di.CoreComponent
 import com.passwordmanager.shared.di.CoreComponentProvider
 import com.passwordmanager.shared.di.CoreModule
 import com.passwordmanager.shared.di.DaggerCoreComponent
 
-class PMApplication : Application(), CoreComponentProvider {
+class PMApplication : Application(), CoreComponentProvider, PMComponentProvider {
 
     override fun getCoreComponent(): CoreComponent {
         return DaggerCoreComponent.builder()
             .coreModule(CoreModule(this))
             .build()
+    }
+
+    override fun getPMComponent(): PMComponent {
+        return DaggerPMComponent.builder()
+            .coreComponent(getCoreComponent())
+            .build()
+
     }
 }
