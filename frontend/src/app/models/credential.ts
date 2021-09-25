2fef36319e36e1154a148311114b7f63c897c042
Sunil Jamod <sunilgj@infusionanalysts.com>
2021-09-25 16:15:45 +0530

    Updated changes

--- frontend/src/app/models/credential.ts
+++ frontend/src/app/models/credential.ts
@@ -1,15 +1,26 @@
 export class Credential {
   id: number;
   name: string;
-  username: string;
+  userName: string;
   password: string;
   description: string;
+  userId: number;
+
+  canView: string;
+  canEdit: string;
+  canDelete: string;
 
   constructor(data) {
     this.id = data.id || 0;
     this.name = data.name || "";
-    this.username = data.username || "";
+    this.userName = data.userName || "";
     this.password = data.password || "";
     this.description = data.description || "";
+
+    this.userId = data.userId || 0;
+
+    this.canView = data.canView || false;
+    this.canDelete = data.canDelete || false;
+    this.canEdit = data.canEdit || false;
   }
 }
